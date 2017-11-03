c5add3c664c8429a54879c98cc1d986c29d0b481
oiu850714 <oiu850714@gmail.com>
2017-11-03 23:49:34 +0800

    add some tcp server socket programming routine in server.cpp

--- HW1/server.cpp
+++ HW1/server.cpp
@@ -14,6 +14,24 @@ using std::string;
 struct client_info
 {
     sockaddr_in client_ip_port;
-    string name;
-    int socket_fd;
+    string name = "anonymous";
+    int client_socket_fd;
+}
+
+int main()
+{
+    //create server socket
+    int server_socket_fd;
+    if( (server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        printf("open socket failed.\n");
+        exit(1);
+    }
+
+    //bind socket
+    sockaddr_in server_ip_port;
+    bzero(server_ip_port, sizeof(server_ip_port));
+    server_ip_port.sin_family = AF_INET;
+    server_ip_port.sin_port = 
+    if(!bind(server_socket_fd, ))
 }
