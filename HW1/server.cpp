28a49bfa28a1d8db7e7cb28bf3f9e196dcf4c94f
oiu850714 <oiu850714@gmail.com>
2017-11-05 17:14:59 +0800

    update server.cpp line 116: forgot to initialize

--- HW1/server.cpp
+++ HW1/server.cpp
@@ -113,7 +113,7 @@ int send_welcome_and_add_client(int server_socket_fd, vector<client_info> &clien
 {
     client_info new_client;
     bzero(&new_client.ip_port, sizeof(sockaddr_in));
-    socklen_t len;
+    socklen_t len = sizeof(sockaddr_in);
     new_client.socket_fd =
         accept(server_socket_fd, (sockaddr*)&new_client.ip_port, &len);
     
