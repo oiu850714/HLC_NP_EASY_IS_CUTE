61fded34980e2cd2be42b910a83c4ff37fca9bee
oiu850714 <oiu850714@gmail.com>
2017-11-05 12:44:10 +0800

    almost complete

--- HW1/server.cpp
+++ HW1/server.cpp
@@ -10,6 +10,9 @@
 #include <sys/select.h>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <cctype>
 
 //#include <Server.h>
 
@@ -18,11 +21,17 @@
 #define SERVER_PROMPT "[Server]"
 #define BROCAST_STR "[Server] Someone is coming!\n"
 #define NEW_COME_STR "[Server] Hello, anonymous! From:"
-
+#define SUCCESS_MSG "[Server] SUCCESS: Your message has been sent.\n"
+#define YOU_ANONYMOUS "[Server] ERROR: You are anonymous.\n"
+#define USER_ANONYMOUS "[Server] ERROR: The client to which you sent is anonymous.\n"
+#define USER_NOT_EXIST "[Server] ERROR: The receiver doesn't exist.\n"
+#define NAME_NOT_2to12_ENG "[Server] ERROR: Username can only consists of 2~12 English letters.\n"
+#define NEW_NAME_ANONYMOUS "[Server] ERROR: Username cannot be anonymous.\n"
 using std::string;
 using std::vector;
 using std::max;
 //using std::to_string;
+using std::stringstream;
 
 struct client_info
 {
@@ -31,12 +40,41 @@ struct client_info
     int socket_fd;
 };
 
-const char* generate_ip_port_pair_str(const client_info& client)
+void generate_ip_port_pair_c_str(const client_info &client, string &ip_port_pair_string)
 {
     char buff[20];
     inet_ntop(AF_INET, &client.ip_port.sin_addr, buff, sizeof(buff));
+    ip_port_pair_string += buff;
+    ip_port_pair_string += "/";
 
-    return ((string(buff) + "/") + std::to_string(htons(client.ip_port.sin_port))).c_str();
+    //printf("buff?!: %s\n\n\n\n", buff);
+    //std::cout << "buff?!: " << buff << "\n\n\n\n";
+
+    sprintf(buff, "%hu", htons(client.ip_port.sin_port));
+    ip_port_pair_string += buff;
+    std::cout << "ip_port_pair_string: " << ip_port_pair_string << "\n";
+    std::cout << "ip_port_pair_string_c_tr: " << ip_port_pair_string.c_str() << "\n";
+    //return ((string(buff) + "/") + std::to_string(htons(client.ip_port.sin_port))).c_str();
+    //return ip_port_pair_string.c_str();
+}
+
+string find_user_name_by_sockfd(vector<client_info> &clients, int client_socket_fd)
+{
+    for(auto client : clients)
+    {
+        if(client.socket_fd == client_socket_fd)
+            return client.name;
+    }
+}
+
+int find_user_name_by_string_name(vector<client_info> &clients, const string &name)
+{
+    for(auto client : clients)
+    {
+        if(client.name == name)
+            return client.socket_fd;
+    }
+    return -1;
 }
 
 int max_fd(vector<client_info> &clients)
@@ -59,10 +97,10 @@ int send_welcome_and_add_client(int server_socket_fd, vector<client_info> &clien
     new_client.socket_fd =
         accept(server_socket_fd, (sockaddr*)&new_client.ip_port, &len);
     
-    string new_come_message = 
-            string(NEW_COME_STR) + generate_ip_port_pair_str(new_client) + "\n";
+    string ip_port_pair_string;
+    generate_ip_port_pair_c_str(new_client, ip_port_pair_string);
 
-    printf("RRRRRRRRRRR: %s\n", generate_ip_port_pair_str(new_client));
+    string new_come_message = NEW_COME_STR + ip_port_pair_string + "\n";
 
     clients.push_back(new_client);
     write(new_client.socket_fd, new_come_message.c_str(), strlen(new_come_message.c_str()));
@@ -72,20 +110,26 @@ int send_welcome_and_add_client(int server_socket_fd, vector<client_info> &clien
 
 void brocast_to_others(int new_client_sockfd, vector<client_info> &clients)
 {
-    for(auto client : clients)
+    for(int i = 0; i < clients.size(); i++)
     {
-        if(client.socket_fd == new_client_sockfd)
+        if(clients[i].socket_fd == new_client_sockfd)
             continue;
         else
         {
-            write(client.socket_fd, BROCAST_STR, strlen(BROCAST_STR));
+            write(clients[i].socket_fd, BROCAST_STR, strlen(BROCAST_STR));
         }
     }
 }
 
 void parse_command_and_send_message(vector<client_info> &clients, char *command_from_client, int client_socket_fd)
 {
-    if(string("who\n") == command_from_client)
+    stringstream SS;
+    SS << command_from_client;
+    std::cout << "command's content: " << SS.str() << "\n";
+
+    string command;
+    SS >> command;
+    if(command == "who")
     {
         //write(client_socket_fd, "WHO!!!\n", strlen("WHO!!!\n"));
         string list;
@@ -95,24 +139,136 @@ void parse_command_and_send_message(vector<client_info> &clients, char *command_
             list += " ";
             list += client.name;
             list += " ";
-            //list += generate_ip_port_pair_str(client);
+            string ip_port_pair_string;
+            //list += generate_ip_port_pair_c_str(client);
+            generate_ip_port_pair_c_str(client, ip_port_pair_string);
+            list += ip_port_pair_string;
             if(client.socket_fd == client_socket_fd)
                 list += " -> me";
             list += "\n";
         }
         write(client_socket_fd, list.c_str(), list.size());
     }
-    else if(string(command_from_client).substr(0, 5) == "yell ")
+    else if(command == "yell")
     {
-        write(client_socket_fd, "YELL!!\n", strlen("YELL!!\n"));
+        string yell_arg;
+        string temp_arg;
+        while(SS)
+        {
+            SS >> temp_arg;
+            yell_arg += temp_arg + " ";
+        }
+
+        //write(client_socket_fd, "YELL!!\n", strlen("YELL!!\n"));
+        string yell_user = find_user_name_by_sockfd(clients, client_socket_fd);
+        for(auto client: clients)
+        {
+            if(client.socket_fd == client_socket_fd)
+            {
+                write(client.socket_fd, SUCCESS_MSG, strlen(SUCCESS_MSG));
+            }
+            else
+            {
+                string yell_msg = SERVER_PROMPT;
+                yell_msg += " ";
+                yell_msg += yell_user;
+                yell_msg += " ";
+                yell_msg += "yell ";
+                //yell_msg += string(command_from_client).substr(5);
+                yell_msg += yell_arg + "\n";
+                write(client.socket_fd, yell_msg.c_str(), yell_msg.size());
+            }
+        }
     }
-    else if(string(command_from_client).substr(0, 5) == "tell ")
+    else if(command == "tell")
     {
-        write(client_socket_fd, "TELL!!\n", strlen("TELL!!\n"));
+        string user_want_to_tell;
+        SS >> user_want_to_tell;
+        int user_want_to_tell_sockfd;
+        //write(client_socket_fd, "TELL!!\n", strlen("TELL!!\n"));
+        if(find_user_name_by_sockfd(clients, client_socket_fd) == "anonymous")
+        {
+            write(client_socket_fd, YOU_ANONYMOUS, strlen(YOU_ANONYMOUS));
+        }
+        else if(user_want_to_tell == "anonymous")
+        {
+            write(client_socket_fd, USER_ANONYMOUS, strlen(USER_ANONYMOUS));
+        }
+        else if((user_want_to_tell_sockfd = 
+                    find_user_name_by_string_name(clients, user_want_to_tell)) == -1)
+        {
+            write(client_socket_fd, USER_NOT_EXIST, strlen(USER_NOT_EXIST));
+        }
+        else
+        {
+            write(client_socket_fd, SUCCESS_MSG, strlen(SUCCESS_MSG));
+            string tell_arg;
+            string temp_arg;
+            while(SS)
+            {
+                SS >> temp_arg;
+                tell_arg += temp_arg + " ";
+            }
+            string tell_msg;
+            tell_msg += "[Server] ";
+            tell_msg += find_user_name_by_sockfd(clients, client_socket_fd) + " ";
+            tell_msg += "tell you ";
+            tell_msg += tell_arg;
+            write(user_want_to_tell_sockfd, tell_msg.c_str(), tell_msg.size());
+        }
     }
-    else if(string(command_from_client).substr(0, 5) == "name ")
+    else if(command ==  "name")
     {
-        write(client_socket_fd, "NAME!!\n", strlen("NAME!!\n"));
+        //write(client_socket_fd, "NAME!!\n", strlen("NAME!!\n"));
+        string name_want_to_change;
+        SS >> name_want_to_change;
+        int i = 0;
+        if(name_want_to_change == "anonymous")
+        {
+            write(client_socket_fd, NEW_NAME_ANONYMOUS, strlen(NEW_NAME_ANONYMOUS));
+            return;
+        }
+        for(; i < name_want_to_change.size(); i++)
+        {
+            if(!isalpha(name_want_to_change[i]))
+                break;
+        }
+        if(i < 2 || i >= 12)
+        {
+            write(client_socket_fd, NAME_NOT_2to12_ENG, strlen(NAME_NOT_2to12_ENG));
+        }
+        else if(find_user_name_by_string_name(clients, name_want_to_change) != -1)
+        {
+            string FUCKING_USED;
+            FUCKING_USED += "[Server] ERROR: ";
+            FUCKING_USED += name_want_to_change + " ";
+            FUCKING_USED += "has been used by others.\n";
+            write(client_socket_fd, FUCKING_USED.c_str(), FUCKING_USED.size());
+        }
+        else
+        {
+            string TELL_OTHER_CHANGE_NAME = "[Server] ";
+            string old_name = find_user_name_by_sockfd(clients, client_socket_fd);
+            TELL_OTHER_CHANGE_NAME += old_name + " ";
+            TELL_OTHER_CHANGE_NAME += "is now known as ";
+            TELL_OTHER_CHANGE_NAME += name_want_to_change + "\n";
+            for(int i = 0; i < clients.size(); i++)
+            {
+                if(client_socket_fd == clients[i].socket_fd)
+                {
+                    clients[i].name = name_want_to_change;
+                }
+                else
+                {
+                    //string TELL_OTHER_CHANGE_NAME = "[Server] <OLD USERNAME> is now known as <NEW USERNAME>.\n";
+                    write(clients[i].socket_fd, TELL_OTHER_CHANGE_NAME.c_str(), TELL_OTHER_CHANGE_NAME.size());        
+                }
+            }
+            string CHANGE_NAME_MSG = "[Server] You're now known as ";
+            CHANGE_NAME_MSG += name_want_to_change + "\n";
+            write(client_socket_fd, CHANGE_NAME_MSG.c_str(), CHANGE_NAME_MSG.size());
+        }
+        
     }
     else
     {
