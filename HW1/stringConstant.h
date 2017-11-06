726c4025f91860ea3308031aa1a72603231f8ba6
oiu850714 <oiu850714@gmail.com>
2017-11-06 10:34:15 +0800

    move server.cpp's define constant to stringConstant.h

---
+++ HW1/stringConstant.h
@@ -0,0 +1,13 @@
+
+#define MAXLINE 2000
+#define SERVERCOUNT 1
+#define SERVER_PROMPT "[Server]"
+#define BROCAST_STR "[Server] Someone is coming!\n"
+#define NEW_COME_STR "[Server] Hello, anonymous! From:"
+#define SUCCESS_MSG "[Server] SUCCESS: Your message has been sent.\n"
+#define YOU_ANONYMOUS "[Server] ERROR: You are anonymous.\n"
+#define USER_ANONYMOUS "[Server] ERROR: The client to which you sent is anonymous.\n"
+#define USER_NOT_EXIST "[Server] ERROR: The receiver doesn't exist.\n"
+#define NAME_NOT_2to12_ENG "[Server] ERROR: Username can only consists of 2~12 English letters.\n"
+#define NEW_NAME_ANONYMOUS "[Server] ERROR: Username cannot be anonymous.\n"
+#define ERR_COMMAND "[Server] ERROR: Error command.\n"
