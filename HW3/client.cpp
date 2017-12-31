6211a39d5fe4cff99f90fcbd0730e603c2bf420a
oiu850714 <oiu850714@gmail.com>
2018-01-01 03:48:15 +0800

    rename HW3's directory name

--- HW3/client.cpp
+++ HW3/client.cpp
@@ -13,180 +13,111 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <cstdio>
 
-#define MAXLINE 1460
 //int initialize_socket(char *ip_str, char *port_str,
 /* value-result arg*/ //int *socket_fd, struct *sockaddr_in sock_struct);
 
 using std::string;
 using std::cin;
+using std::cout;
 using std::vector;
 using std::stringstream;
 using std::max;
+using std::to_string;
 
-struct transfer_record
+#include "defineStruct.h"
+#include "defineConstant.h"
+#include "functionUtil.h"
+
+
+int max_fd(vector<connection> &connections)
 {
-    int64_t request; 
-    /*-1: constructor state
-     * 0: greeting
-     * 1: client put command, file transfer request
-     * 2: server reply for put
-     * 3: get file from server request when login
-     * 4: server reply for get
-     * 5: server actively says there is a file for you
-     * 6: client reply for 5
-     * 7: payload is file content
-    */
-    char payload[MAXLINE];
-    /*-1: no content
-     * 0: greeting string
-     * 1: filename
-     * 2: no content
-     * 3: filename
-     * 4: no content
-     * 3: file content
-    */
-    transfer_record()
+    int temp_fd = 0;
+    //printf("temp_fd: %d\n", temp_fd);
+    for(auto  connection: connections)
     {
-        request = -1;
-        payload[0] = '\0';
+        if(connection.socket_fd > temp_fd)
+            temp_fd = connection.socket_fd;
     }
-};
-
-class nonblockingSocket
-{
-private:
-    char buffer_to_dest[MAXLINE];
-    char *to_dest_remain_ptr; // point to address that can insert data from file
-    char *to_dest_ready_ptr; // point to data that haven't been sent to server
-
-    char buffer_from_dest[MAXLINE];
-    char *from_dest_remain_ptr; // point to address that can insert date from server
-    char *from_dest_ready_ptr; // point to data that haven't been sent to local file
-
-    FILE *fp;
-    string filename;
-    struct transfer_record record;
-    int64_t start_session_type;
-
-public:
-    int socket_fd;
-    nonblockingSocket(char *IP_str, char *port_str, int64_t session_type);
-    void set_to_nonblocking();
-    void open_upload_file(string filename);
-    void send_transfer_request()
-    {
-
-    }
-    void send_data(int length_of_send_data, char *src);
-    void receive_data(int &length_of_receive_data, char *dest);
-
-
-    //need close function destructor?
-
-private:
-    int new_tcp_connection(char *IP_str, char *port_str);
-};
-
-nonblockingSocket::nonblockingSocket(char *IP_str, char *port_str, int64_t session_type)
-{
-    fp = NULL;
-    socket_fd = new_tcp_connection(IP_str, port_str);
-    to_dest_ready_ptr = to_dest_remain_ptr = buffer_to_dest;
-    from_dest_ready_ptr = from_dest_remain_ptr = buffer_from_dest;
-    // initialize variable
-
-
-    session_type ? set_to_nonblocking() : false;
-    // 改成看這個 type 決定這個 socket 的起始狀態是哪個
+    return temp_fd;
 }
 
-void nonblockingSocket::set_to_nonblocking()
+int main(int argc, char **argv)
 {
-    int fcntl_val = fcntl(socket_fd, F_GETFL, 0);
-    fcntl(socket_fd, F_SETFL, fcntl_val | O_NONBLOCK);
-    // set socket to nonblocking
-}
-
-int nonblockingSocket::new_tcp_connection(char *IP_str, char *port_str)
-{
-    int socket_fd;
-    if( (socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        printf("open socket failed.\n");
-        exit(1);
-    }
-
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port =  htons(atoi(port_str));
-
-    if(!inet_pton(AF_INET, IP_str, &server_addr.sin_addr))
-    {
-        printf("fuckyou invalid IP address\n");
-        exit(1);
-    }
-
-    if (connect(socket_fd, (sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-    {
-        printf("connect error\n");
-        exit(1);
-    }
-    return socket_fd;
-}
-
-void nonblockingSocket::open_upload_file(string filename)
-{
-    if( (fp = fopen(filename.c_str(), "r")) < 0)
-    {
-        std::cout << "openfile error\n";
-        exit(1);
-    }
-}
-
-#define BLOCKING false
-
-int main(int argc, char ** argv)
-{
-
     if(argc < 4)
     {
         printf("./client <ip> <port> <username>");
         return 1;
     }
 
-    nonblockingSocket main_socket(argv[1], argv[2], BLOCKING);
+    string socket_string(argv[1]);
+    string socket_port(argv[2]);
+    string username(argv[3]);
 
-    /*
-    int main_socket_fd = new_tcp_connection(argv[1], argv[2]);
-    int fcntl_val = fcntl(main_socket_fd, F_GETFL, 0);
-    fcntl(main_socket_fd, F_SETFL, fcntl_val | O_NONBLOCK);
-    */
+    connection main_socket_connection;
+    main_socket_connection.socket_fd = new_tcp_connection(argv[1], argv[2]);
+    connection upload_file_connection;
+    upload_file_connection.socket_fd = new_tcp_connection(argv[1], argv[2]);
 
-    int n;
-    char recvline[MAXLINE];
-    recvline[0] = 0;
+    main_socket_connection.set_username(username);
+    main_socket_connection.set_request_type(CLI_RQ_USERNAME);
+    cout << "main request type: " << main_socket_connection.get_request_type() << "\n";
+
+    upload_file_connection.set_username(username);
+    upload_file_connection.set_request_type(CLI_RQ_UPLOAD_CONNECTION);
+    cout << "upload request type: " << upload_file_connection.get_request_type() << "\n";
+    
+    while(true)
+    {
+        main_socket_connection.write_to_socket();
+        if(main_socket_connection.is_write_completely_packet())
+        {
+            main_socket_connection.reset_buffer_and_fill_member_from_packet();
+            break;
+        }
+    }
+
+    while(true)
+    {
+        upload_file_connection.write_to_socket();
+        if(upload_file_connection.is_write_completely_packet())
+        {
+            upload_file_connection.reset_buffer_and_fill_member_from_packet();
+            break;
+        }
+    }
+
+
+    cout << "Welcome to the dropbox-like server! : " << argv[3] << "\n";
+
+    vector<connection> connections;
     char commandline[MAXLINE];
-    commandline[0] = 0;
-    vector<nonblockingSocket> download_file_connections;
-    vector<nonblockingSocket> upload_file_connections;
 
     while (true) 
     {
         fd_set reading_fds;
+        fd_set writing_fds;
         struct timeval tv;
     
         tv.tv_sec = 0;
         tv.tv_usec = 500;
         FD_ZERO(&reading_fds);
         FD_SET(STDIN_FILENO, &reading_fds);
-        FD_SET(main_socket.socket_fd, &reading_fds);
-        int maxfdp = max(STDIN_FILENO, main_socket.socket_fd) + 1;
-        // select only for stdin
+        FD_SET(main_socket_connection.socket_fd, &reading_fds);
+        FD_SET(upload_file_connection.socket_fd, &writing_fds);
+
+        for (auto file_connection : connections)
+        {
+            FD_SET(file_connection.socket_fd, &reading_fds);
+        }
+        int maxfdp = max(
+                max(main_socket_connection.socket_fd, upload_file_connection.socket_fd), 
+                max_fd(connections)) + 1;
 
         int new_command_flag = 0, server_reply_flag = 0;
-        switch (select(maxfdp, &reading_fds, NULL, NULL, &tv))
+
+        switch (select(maxfdp, &reading_fds, &writing_fds, NULL, &tv))
         {
             case -1:
                 exit(-1);
@@ -201,20 +132,113 @@ int main(int argc, char ** argv)
                         exit(0);
                     // read commandline, but the content will be parsed later
                 }
-                if(FD_ISSET((main_socket.socket_fd, &reading_fds))
+                if(FD_ISSET(main_socket_connection.socket_fd, &reading_fds))
                 {
-                    server_reply_flag = 1;
-                    //read to main_socket's record
+                    int n = main_socket_connection.read_from_socket();
+                    if(n == 0)
+                    {
+                        cout << "server close connection.\n";
+                        exit(0);
+                    }
+                    if(main_socket_connection.can_parse_packet())
+                    {
+                        // reset buffer pointer and read data length
+                        main_socket_connection.reset_buffer_and_fill_member_from_packet();
+                        
+                        int32_t server_request = main_socket_connection.get_request_type();
+                        
+                        //read to main_socket's record
+                        switch(server_request)
+                        {
+                            case SERV_RQ_DOWNLOAD:
+                                connections.push_back(connection());
+                                connections.back().set_sockaddr(main_socket_connection.get_sockaddr());
+                                connections.back().set_filename(main_socket_connection.get_filename());
+                                connections.back().openfile_to_write();
+                                connections.back().connect_by_sockaddr_in();
+                                break;
+                        }
+                    }
                 }
-                if()
+                
+                if(FD_ISSET(upload_file_connection.socket_fd, &writing_fds))
                 {
-                    
+                    if(!upload_file_connection.file_is_close())
+                    {
+                        upload_file_connection.write_to_socket();
+
+                        if(upload_file_connection.is_write_completely_packet())
+                        {
+                            upload_file_connection.reset_buffer_and_fill_member_from_packet();
+                            if(upload_file_connection.get_request_type() != CLI_RQ_UPFIN)
+                            {   
+                                upload_file_connection.read_payload_from_file();
+                                cout << "read payload len: " << upload_file_connection.get_payload_len() << "\n";
+                                // may read 0 byte, which means EOF(or fucking error), and payload_len will be set to 0
+                                if(upload_file_connection.get_payload_len() == 0)
+                                {
+                                    cout << "file content all been read\n";
+                                    upload_file_connection.set_request_type(CLI_RQ_UPFIN);
+                                }
+                                else
+                                {
+                                    upload_file_connection.set_request_type(CLI_RQ_UPLOADING);
+                                }
+                            }
+                            else
+                            {
+                                cout << "close file\n";
+                                upload_file_connection.close_file();
+                            }
+                        }
+                    }
                 }
-            break;
+
+                for(auto &file_connection : connections)
+                {
+                    if(FD_ISSET(file_connection.socket_fd, &reading_fds))
+                    {
+                        int n = file_connection.read_from_socket();
+                        if(n == 0)
+                        {
+                            cout << "server close connection.\n";
+                            exit(0);
+                        }
+                        if(file_connection.can_parse_packet())
+                        {
+                            //server send file content
+                            file_connection.reset_buffer_and_fill_member_from_packet();
+                            int32_t server_request = file_connection.get_request_type(); // "0", "1", ...
+                            switch(server_request)
+                            {
+                                case SERV_RQ_DOWNLOADING:
+                                    file_connection.wrtie_payload_to_file();
+                                    break;
+                                case SERV_RQ_DOWNFIN:
+                                    file_connection.close_file();
+                                    file_connection.close_socket();
+                                    break;
+                            }
+                        }
+                    }
+                }
+                for(int i = 0; i < connections.size();)
+                {
+                    if(connections[i].file_is_close())
+                    {
+                        connections.erase(connections.begin() + i);
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                break;
         }
 
         if(new_command_flag)
-        {   
+        {
+            new_command_flag = 0;   
             stringstream SS;
             SS << commandline;
             string command;
@@ -223,6 +247,22 @@ int main(int argc, char ** argv)
             {
                 string filename;
                 SS >> filename;
+
+                upload_file_connection.set_request_type(CLI_RQ_UPLOADFILE);
+                upload_file_connection.set_filename(filename);
+                upload_file_connection.set_username(username);
+                upload_file_connection.openfile_to_read();
+                upload_file_connection.write_to_socket();
+                upload_file_connection.reset_buffer_and_fill_member_from_packet();
+                //反正一 write 之後，object 內部指向 buffer 的指標就會被更動，不管怎樣都要 reset buffer
+
+                //先讀第一段 file content，這樣上面的 FD_ISSET(upload_file_connection.socket_fd, &writing_fds) 比較好寫
+                upload_file_connection.read_payload_from_file();
+                upload_file_connection.set_request_type(CLI_RQ_UPLOADING);
+
+                /*
+                transfer_packet file_put_request_packet(CLI_RQ_UPLOADFILE, argv[3], filename.c_str(), NULL, 0, NULL);
+                */
             }
             else if(string("/sleep") == command)
             {
@@ -233,9 +273,9 @@ int main(int argc, char ** argv)
                 {
                     counter++;
                     sleep(1);
-                    std::cout << "Sleep " << counter << "\n";
+                    cout << "Sleep " << counter << "\n";
                 }
-                std::cout << "Client wakes up\n";
+                cout << "Client wakes up\n";
             }
             else if(string("/exit") == command)
             {
@@ -243,4 +283,5 @@ int main(int argc, char ** argv)
             }
         }
     }
+
 }
